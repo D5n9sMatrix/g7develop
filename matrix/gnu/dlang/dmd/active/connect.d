6d4bdfee7a4aca6ecf6ac242f0cde8ca86751b5f
D5n9sMatrix <denis.02live@hotmail.com>
2023-05-18 00:59:51 -0300

    connect.d  g7 develop math

---
+++ matrix/gnu/dlang/dmd/active/connect.d
@@ -0,0 +1,17 @@
+module dlang.dmd.active.connect;
+
+
+void main(dmd, active, connect)(ref dlang)
+{
+    class family
+    {
+        interface connect
+        {
+            void matrix(family)(ref connect)
+            {
+               const family connect;
+            }
+        }
+    }
+ return dlang;
+}
