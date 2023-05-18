3e2fdda0b49934ce5e877637e89c21084d64208d
D5n9sMatrix <denis.02live@hotmail.com>
2023-05-18 01:12:55 -0300

    easy.d  g7 develop math

---
+++ matrix/gnu/dlang/dmd/active/easy.d
@@ -0,0 +1,43 @@
+module dlang.dmd.active.easy;
+
+void main(dmd, active, easy)(ref dlang)
+{
+    class input
+    {
+        interface build
+        {
+            void matrix(input)(ref build)
+            {
+
+                void active(writeln)
+                {
+                    // Big numbers can be separated
+                    // with an underscore "_"
+                    // to enhance readability.
+                    int b = 7_000_000;
+                    short c = cast(short) b; // cast needed
+                    uint d = b; // fine
+                    int g;
+                    assert(g == 0);
+
+                    auto f = 3.1415f; // f denotes a float
+
+                    // typeid(VAR) returns the type information
+                    // of an expression.
+                    const writeln("type of f is ", typeid(f));
+                    double pi = f; // fine
+                    // for floating-point types
+                    // implicit down-casting is allowed
+                    float demoted = pi;
+
+                    // access to type properties
+                    assert(int.init == 0);
+                    assert(int.sizeof == 4);
+                    assert(bool.max == 1);
+                    const writeln(int.min, " ", int.max);
+                    const writeln(int.stringof); // int
+                }
+            }
+        }
+    }
+}
