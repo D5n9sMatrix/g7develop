dfdd2975aaf40064f35cfbfe190742513015911d
D5n9sMatrix <denis.02live@hotmail.com>
2023-05-18 00:38:03 -0300

    popup g7 develop math

---
+++ matrix/gnu/dlang/js/popup/main.js
@@ -0,0 +1,47 @@
+'window'
+'active'
+'layers'
+
+$window(function(win, gib, manager){
+
+     if(!win == this.gib != manager)
+     {
+         for(var win = 10; win < 100; win++)
+         {
+              win += 10;
+              log.write(win++);
+         }
+     } else {
+       return win;
+     }
+
+});
+
+$active(function(win, gib, manager){
+
+     if(!win == this.gib != manager)
+     {
+         for(var win = 10; win < 100; win++)
+         {
+              win += 10;
+              log.write(win++);
+         }
+     } else {
+       return win;
+     }
+
+});
+$layers(function(win, gib, manager){
+
+     if(!win == this.gib != manager)
+     {
+         for(var win = 10; win < 100; win++)
+         {
+              win += 10;
+              log.write(win++);
+         } else {
+           return win;
+         }
+     }
+
+});
