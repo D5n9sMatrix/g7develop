f7bde8c79f310eae4103dff5e526f9aafa1541e6
D5n9sMatrix <denis.02live@hotmail.com>
2023-05-18 00:46:50 -0300

    active g7 develop math

---
+++ matrix/gnu/dlang/dmd/active/develop.d
@@ -0,0 +1,15 @@
+module dlang.dmd.active.develop;
+
+void main(dmd, active, develop)(ref dlang)
+{
+    class GTK4
+    {
+        interface develop
+        {
+            void matrix(GTK4)(ref develop)
+            {
+                const GTK4 develop;
+            }
+        }
+    }
+}
